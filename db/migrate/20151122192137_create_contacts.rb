7c743144336edd91196f871c5bb7d269b2f9a32a
Dian Pan <dian.di.pan@gmail.com>
2015-11-22 11:53:35 -0800

    contacts migration

--- db/migrate/20151122192137_create_contacts.rb
+++ db/migrate/20151122192137_create_contacts.rb
@@ -1,11 +1,11 @@
 class CreateContacts < ActiveRecord::Migration
   def change
     create_table :contacts do |t|
-      t.string :name
-      t.string :email
+      t.string :name, null:false
+      t.string :email, null:false
       t.string :comments
 
-      t.timestamps
+      t.timestamps null:false
     end
   end
 end
