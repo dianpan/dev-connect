f27521d13fa6da6ba72823c658589287ec3a2083
Dian Pan <dian.di.pan@gmail.com>
2015-11-22 19:21:21 -0800

    fix code typos

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,10 +1,11 @@
 class ContactMailer < ActionMailer::Base
   default to: 'dian.di.pan@gmail.com'
 
-  def contact_mailer(name, email, body)
+  def contact_email(name, email, body)
     @name = name
     @email = email
     @body = body
+
     mail(from: email, subject: 'Contact Form Message')
   end
 end
