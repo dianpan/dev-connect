ac44f0f967ffb998ee7e5f6ff1a5cacead4c39cd
Dian Pan <dian.di.pan@gmail.com>
2015-11-30 15:25:55 -0800

    add edit action in profile controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -15,6 +15,9 @@ class ProfilesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email,:description)
